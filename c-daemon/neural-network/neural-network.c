d03f2f1ac897c08f724c671650087c1b162d5d03
Shariele <shariele@localhost.localdomain>
2018-08-29 01:05:28 +0200

    Continued work on back-prop

--- c-daemon/neural-network/neural-network.c
+++ c-daemon/neural-network/neural-network.c
@@ -25,17 +25,17 @@
 /*	TYPE DEFINITIONS & VARIABLES										  */
 /**************************************************************************/
 #define DEBUG 	0
-#define NUDGEDEBUG 0
+#define NUDGEDEBUG 1
 #define SHOWCASE 1
 #define INPUT 	784
 #define HIDDEN 	16	
 #define OUTPUT	10
 
-static double learningRate = 0.1;
-static double eta = 0.5, alpha = 0.9;
+static double learningRate = 0.05;
+// static double eta = 0.5, alpha = 0.9;
 //The highest double value that can ge generated.
 static double max = 2;		
-static double biasMax = 1;	
+static double biasMax = 2;	
 
 
 // Implicit initialization to 0
@@ -81,7 +81,7 @@ static void genMatrix(int sizeX, int sizeY, double matrix[sizeX][sizeY]){
 
 	for(i = 0; i < sizeX; i++){
 		for(j = 0; j < sizeY; j++){
-			matrix[i][j] = (double)((double)rand()/(double)(RAND_MAX/max) - 1);
+			matrix[i][j] = (double)((double)rand()/(double)(RAND_MAX/max) - (max - 1));
 		}
 	}
 }
@@ -90,7 +90,7 @@ static void genBias(double bias[], int size){
 	int i;
 
 	for(i = 0; i < size; i++){
-		bias[i] = (double)((double)rand()/(double)(RAND_MAX/biasMax));
+		bias[i] = (double)((double)rand()/(double)(RAND_MAX/biasMax) - (biasMax - 1));
 	}
 }
 
@@ -125,7 +125,7 @@ static double matrixVectorMult(int i, int j, int matSize, double *vec, double ma
 static void sumLayer(int sourceSize, int wSize, int destSize, double *sourceL, double weigth[destSize][wSize], double *destL,  double *bias){
 	
 	for(int i = 0; i < destSize; i++){
-		destL[i] = sigmoid(matrixVectorMult(i, 0, wSize, sourceL, weigth, 0.0) - bias[i]);
+		destL[i] = sigmoid(matrixVectorMult(i, 0, wSize, sourceL, weigth, 0.0) + bias[i]);
 		// destL[i] = matrixVectorMult(i, 0, wSize, sourceL, weigth, 0.0) - bias[i];
 
 		if(DEBUG)	printf("\ndestL[%d]: %f \t bias[%d]: %f\n\n", i, destL[i], i, bias[i]);
@@ -147,7 +147,11 @@ static void backPropagate(double *input, double *nudgesHO){
 	for(i = 0; i < HIDDEN; i++){
 		sumNudges[i] = 0.0;
 		for(j = 0; j < OUTPUT; j++){
-			sumNudges[i] += W3[i][j]*nudgesHO[j];
+			// sumNudges[i] += W3[i][j]*nudgesHO[j];
+			if(nudgesHO[j] < 0.0)
+				sumNudges[i] += -sqrt(pow((W3[i][j]*nudgesHO[j]), 2));
+			else
+				sumNudges[i] += sqrt(pow((W3[i][j]*nudgesHO[j]), 2));
 		}
 		nudgesHH[i] = sumNudges[i] * H2[i] * (1.0 - H2[i]);		//Används för W2
 	}
@@ -155,27 +159,47 @@ static void backPropagate(double *input, double *nudgesHO){
 	for(i = 0; i < HIDDEN; i++){
 		sumNudges[i] = 0.0;
 		for(j = 0; j < HIDDEN; j++){
-			sumNudges[i] += W2[j][i]*nudgesHH[i];
+			// sumNudges[i] += W2[j][i]*nudgesHH[i];
+			if(nudgesHH[j] < 0.0)
+				sumNudges[i] += -sqrt(pow((W2[i][j]*nudgesHH[j]), 2));
+			else
+				sumNudges[i] += sqrt(pow((W2[i][j]*nudgesHH[j]), 2));
 		}
 		nudgesIH[i] = sumNudges[i] * H1[i] * (1.0 - H1[i]);		//Används för W1
 	}
 
 	// Change biases
-	for(i = 0; i < HIDDEN; i++){
-		sumNudges[i] = 0.0;
-		for(j = 0; j < OUTPUT; j++){
-			sumNudges[i] += W3[i][j]*nudgesHO[j];			//Use sumnudges as a counter
-		}
-		BH2[i] += sumNudges[i];		//Används för W2
-	}
+	// for(i = 0; i < HIDDEN; i++){
+	// 	sumNudges[i] = 0.0;
+	// 	for(j = 0; j < OUTPUT; j++){
+	// 		// sumNudges[i] += (W3[i][j]*nudgesHO[j]);			//Use sumnudges as a counter
+	// 		if(nudgesHO[j] < 0.0)
+	// 			sumNudges[i] += -sqrt(pow((W3[i][j]*nudgesHO[j]), 2));
+	// 		else
+	// 			sumNudges[i] += sqrt(pow((W3[i][j]*nudgesHO[j]), 2));
+	// 	}
+	// 	BH2[i] += sumNudges[i] * H2[i] * (1.0 - H2[i]) * learningRate;		//Används för W2
+	// }
+
+	// for(i = 0; i < HIDDEN; i++){
+	// 	sumNudges[i] = 0.0;
+	// 	for(j = 0; j < HIDDEN; j++){
+	// 		// sumNudges[i] += (W3[i][j]*nudgesHO[j]);			//Use sumnudges as a counter
+	// 		if(nudgesHH[j] < 0.0)
+	// 			sumNudges[i] += -sqrt(pow((W2[i][j]*nudgesHH[j]), 2));
+	// 		else
+	// 			sumNudges[i] += sqrt(pow((W2[i][j]*nudgesHH[j]), 2));
+	// 	}
+	// 	BH1[i] += sumNudges[i] * H1[i] * (1.0 - H1[i]) * learningRate;		//Används för W2
+	// }
 	
-	for(i = 0; i < HIDDEN; i++){
-		sumNudges[i] = 0.0;
-		for(j = 0; j < OUTPUT; j++){
-			sumNudges[i] += W2[i][j]*nudgesHH[j];			//Use sumnudges as a counter
-		}
-		BH1[i] += sumNudges[i];		//Används för W
-	}
+	// for(i = 0; i < HIDDEN; i++){
+	// 	sumNudges[i] = 0.0;
+	// 	for(j = 0; j < OUTPUT; j++){
+	// 		sumNudges[i] += (-W2[i][j]*nudgesHH[j]);			//Use sumnudges as a counter
+	// 	}
+	// 	BH1[i] += sumNudges[i];		//Används för W
+	// }
 
 	if(NUDGEDEBUG){
 		for(i = 0; i < OUTPUT; i++){
@@ -191,125 +215,28 @@ static void backPropagate(double *input, double *nudgesHO){
 		}
 	}
 
-	// W3 
+	// // W3 
 	for(i = 0; i < OUTPUT; i++){
 		for(j = 0; j < HIDDEN; j++){
-			DeltaW3[j][i] = learningRate * H2[i] * nudgesHO[i] + learningRate * DeltaW3[i][j];
+			DeltaW3[j][i] = learningRate * H2[i] * nudgesHO[i] + learningRate * DeltaW3[j][i];
 			W3[j][i] += DeltaW3[j][i];
 		}
 	}
-	// W2
+	// // W2
 	for(i = 0; i < HIDDEN; i++){
 		for(j = 0; j < HIDDEN; j++){
-			DeltaW2[j][i] = learningRate * H1[i] * nudgesHH[i] + learningRate * DeltaW2[i][j];
-			W2[j][i] += DeltaW2[j][i];
+			DeltaW2[i][j] = learningRate * H1[i] * nudgesHH[i] + learningRate * DeltaW2[i][j];
+			W2[i][j] += DeltaW2[i][j];
 		}
 	}
 
-	// W1
-	for(i = 0; i < INPUT; i++){
-		for(j = 0; j < HIDDEN; j++){
-			DeltaW1[j][i] = learningRate * input[i] * nudgesIH[i] + learningRate * DeltaW1[j][i];
-			W1[j][i] += DeltaW1[j][i];
+	// // W1
+	for(i = 0; i < HIDDEN; i++){
+		for(j = 0; j < INPUT; j++){
+			DeltaW1[i][j] = learningRate * input[i] * nudgesIH[i] + learningRate * DeltaW1[i][j];
+			W1[i][j] += DeltaW1[i][j];
 		}
 	}
-
-
-	// W3, BH2 is handled here.
-	// for(i = 0; i < OUTPUT; i++){
-	// 	learningValue 		 = sqrt(pow((nudgesHO[i]*learningRate), 2));	//Make a positive number.
-	// 	// learningValue 		 = 	 (nudges[i]*learningRate);
-	// 	// if(DEBUG) printf("\nNudge[%d] from output is: %f\n", i, nudges[i]);
-		
-	// 	// Start with W3, H2, BH2
-	// 	// The greater the influence of the node in H2, the greater the increase/decrease in value to maximize
-	// 	// the "bang for our bucks" on the weight/bias.
-	// 	for(j = 0; j < HIDDEN; j++){
-	// 		if(DEBUG) printf("Weight3 [%d][%d] was: %f\n", j, i, W3[j][i]);
-
-	// 		if(nudgesHO[i] < 0.0){							//If the nudge is negative, this is not the desired output. Decrease impact of edges.
-	// 			if(W3[j][i] < 0.0)
-	// 				W3[j][i] 		+= learningValue*Out[i];	//Negative becomes less negative.
-	// 			else 
-	// 				W3[j][i] 		-= learningValue*Out[i];	//Positive becomes less positive.
-
-	// 			// If H2[j] had a huge impact leading to a wrong answer (i.e. negative nudge), increase bias to make it less
-	// 			// likely to affect to a wrong answer again.
-	// 			BH2[j]				+= learningValue;
-	// 			// Remember desired changes
-	// 			layerNudges[j]		+= learningValue*W3[j][i];
-	// 		}else{											//If the nudge is positive, this is desired output. Increase positive impact of edges.
-	// 			W3[j][i] 			+= learningValue*Out[i];	//Positive becomes more positive.
-	// 			BH2[j]				-= learningValue;
-	// 			// Remember desired changes in proportion to the impact of a given weight.
-	// 			layerNudges[j]		+= learningValue*W3[j][i];
-	// 		}
-
-
-	// 		if(DEBUG) printf("Weight3 [%d][%d] is now: %f\n", j, i, W3[j][i]);
-			
-	// 	}
-
-	// 	if(DEBUG) printf("layerNudge[%d] is: %f\n", i, layerNudges[i]);
-	// }
-	
-	// if(DEBUG) printf("\n\n");
-
-	// // W2, H1, BH1 is handled here.
-	// for(i = 0; i < HIDDEN; i++){
-	// 	learningValue		 = sqrt(pow((layerNudges[i]*learningRate), 2));
-	// 	layerNudges[i]		 = 0.0;
-
-	// 	for(j = 0; j < HIDDEN; j++){
-	// 		if(DEBUG) printf("Weight2 [%d][%d] was: %f\n", j, i, W2[j][i]);
-
-	// 		if(layerNudges[i] < 0.0){							//If the nudge is negative, this is not the desired output. Decrease impact of edges.
-	// 			if(W2[j][i] < 0.0)
-	// 				W2[j][i] 		+= learningValue*H2[j];	//Negative becomes less negative.
-	// 			else 
-	// 				W2[j][i] 		-= learningValue*H2[j];	//Positive becomes less positive.
-
-	// 			// If H2[j] had a huge impact leading to a wrong answer (i.e. negative nudge), increase bias to make it less
-	// 			// likely to affect to a wrong answer again.
-	// 			BH1[j]				+= learningValue;
-	// 			// Remember desired changes
-	// 			layerNudges[j]		-= learningValue*W2[j][i];
-	// 		}else{											//If the nudge is positive, this is desired output. Increase impact of edges.
-	// 			W2[j][i] 			+= learningValue*H2[j];	//Positive becomes more positive.
-	// 			BH1[j]				-= learningValue;
-	// 			// Remember desired changes
-	// 			layerNudges[j]		+= learningValue*W2[j][i];
-	// 		}
-
-
-	// 		if(DEBUG) printf("Weight2 [%d][%d] is now: %f\n", j, i, W2[j][i]);
-	// 	}
-
-	// 	if(DEBUG) printf("layerNudge[%d] is: %f\n", i, layerNudges[i]);
-	// }
-
-	// if(DEBUG) printf("\n\n");
-
-	// // W1 is handeled here. Can only change W1 on this stage.
-	// for(i = 0; i < HIDDEN; i++){
-	// 	learningValue		 = sqrt(pow((layerNudges[i]*learningRate), 2));
-	// 	layerNudges[i]		 = 0.0;
-
-	// 	for(j = 0; j < HIDDEN; j++){
-	// 		if(DEBUG) printf("Weight1 [%d][%d] was: %f\n", j, i, W1[j][i]);
-
-	// 		if(layerNudges[i] < 0.0){
-	// 			if(W1[j][i] < 0.0)
-	// 				W1[j][i]		+= learningValue*H1[j];
-	// 			else
-	// 				W1[j][i] 		-= learningValue*H1[j];
-	// 		}else
-	// 			W1[j][i] 			+= learningValue*H1[j];
-			
-	// 		if(DEBUG) printf("Weight1 [%d][%d] is now: %f\n", j, i, W1[j][i]);
-	// 	}
-
-	// }
 }
 
 // Calculate how much each output was wrong. This will determine how much to change weights, biases associated with this output.
@@ -320,17 +247,17 @@ static void initBackPropagate(double *input, int wantNr, int size){
 
 	for(i = 0; i < size; i++){
 		if(i != wantNr)
-			nudges[i] = -pow((Out[i] - correctOut[i]), 2);
+			nudges[i] = -pow((Out[i] - correctOut[i]), 2) * Out[i] * (1.0 - Out[i]);
 		else
-			nudges[i] = pow((Out[i] - correctOut[i]), 2);
+			nudges[i] = pow((Out[i] - correctOut[i]), 2) *  Out[i] * (1.0 - Out[i]);
 		
 		//	DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]) ;   /* Sigmoidal Outputs, SSE */
-		if(NUDGEDEBUG) printf("correctout[%d]: %f\n", i, correctOut[i]);
+		// if(NUDGEDEBUG) printf("correctout[%d]: %f\n", i, correctOut[i]);
 
-		// nudges[i] = pow((correctOut[i] - Out[i]), 2) /*+  Out[i] * (1.0 - Out[i])*/;
+		// nudges[i] = pow((correctOut[i] - Out[i]), 2) +  Out[i] * (1.0 - Out[i]);
 		correctOut[i] = 0;													//Reset
 
-		if(DEBUG) printf("Outputnudges[%d]: %f\n", i, nudges[i]);
+		// if(NUDGEDEBUG) printf("Outputnudges[%d]: %f\n", i, nudges[i]);
 	}
 
 	backPropagate(input, nudges);
@@ -369,8 +296,8 @@ static void init(){
 	genMatrix(INPUT, HIDDEN, W1);
 	genMatrix(HIDDEN, HIDDEN, W2);
 	genMatrix(HIDDEN, OUTPUT, W3);
-	genBias(BH1, HIDDEN);
-	genBias(BH2, HIDDEN);
+	// genBias(BH1, HIDDEN);
+	// genBias(BH2, HIDDEN);
 }
 
 static mnist_data *importMnistData(){
@@ -482,13 +409,17 @@ int main(int argc, char const *argv[]){
 			for(i = 0; i < HIDDEN; i++)
 				for(j = 0; j < OUTPUT; j++)
 					printf("W3[%d][%d]: %f \n", i, j, W3[i][j]);
-
+			printf("\n");
+			for (int i = 0; i < HIDDEN; i++){
+				printf("H1[%d]: %f \n", i, H1[i]);
+			}
+			printf("\n");
 			for (int i = 0; i < HIDDEN; i++){
 				printf("H2[%d]: %f \n", i, H2[i]);
 			}
 			printf("\n");
-			for (int i = 0; i < HIDDEN; i++){
-				printf("H1[%d]: %f \n", i, H1[i]);
+			for (int i = 0; i < OUTPUT; i++){
+				printf("Out[%d]: %f \n", i, Out[i]);
 			}
 			printf("\n");
 			for (int i = 0; i < HIDDEN; i++){
@@ -509,3 +440,218 @@ int main(int argc, char const *argv[]){
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// V1
+// static void backPropagate(double *input, double *nudgesHO){
+// 	int i, j;
+// 	double 	//learningValue = 0.0,
+// 			sumNudges[HIDDEN],
+// 			nudgesHH[HIDDEN] = {0.0},
+// 			// layerNudges[HIDDEN] = {0.0},
+// 			nudgesIH[HIDDEN] = {0.0};	//Will be used to determine if a layer received a negative or positive nudge when summed up. e.g. Out -> H2.
+// 										//Will be used to calculate the next layer, weights and biases.
+
+// 	// NudgesHO används for W3
+// 	// Prepare nudges
+// 	for(i = 0; i < HIDDEN; i++){
+// 		sumNudges[i] = 0.0;
+// 		for(j = 0; j < OUTPUT; j++){
+// 			sumNudges[i] += W3[i][j]*nudgesHO[j];
+// 		}
+// 		nudgesHH[i] = sumNudges[i] * H2[i] * (1.0 - H2[i]);		//Används för W2
+// 	}
+
+// 	for(i = 0; i < HIDDEN; i++){
+// 		sumNudges[i] = 0.0;
+// 		for(j = 0; j < HIDDEN; j++){
+// 			sumNudges[i] += W2[j][i]*nudgesHH[i];
+// 		}
+// 		nudgesIH[i] = sumNudges[i] * H1[i] * (1.0 - H1[i]);		//Används för W1
+// 	}
+
+// 	// Change biases
+// 	for(i = 0; i < HIDDEN; i++){
+// 		sumNudges[i] = 0.0;
+// 		for(j = 0; j < OUTPUT; j++){
+// 			sumNudges[i] += W3[i][j]*nudgesHO[j];			//Use sumnudges as a counter
+// 		}
+// 		BH2[i] += sumNudges[i];		//Används för W2
+// 	}
+	
+// 	for(i = 0; i < HIDDEN; i++){
+// 		sumNudges[i] = 0.0;
+// 		for(j = 0; j < OUTPUT; j++){
+// 			sumNudges[i] += W2[i][j]*nudgesHH[j];			//Use sumnudges as a counter
+// 		}
+// 		BH1[i] += sumNudges[i];		//Används för W
+// 	}
+
+// 	if(NUDGEDEBUG){
+// 		for(i = 0; i < OUTPUT; i++){
+// 			printf("NudgeHO[%d]: %f\n", i, nudgesHO[i]);
+// 		}
+// 		printf("\n");
+// 		for(i = 0; i < HIDDEN; i++){
+// 			printf("NudgeHH[%d]: %f\n", i, nudgesHH[i]);
+// 		}
+// 		printf("\n");
+// 		for(i = 0; i < HIDDEN; i++){
+// 			printf("NudgeIH[%d]: %f\n", i, nudgesIH[i]);
+// 		}
+// 	}
+
+// 	// W3 
+// 	for(i = 0; i < OUTPUT; i++){
+// 		for(j = 0; j < HIDDEN; j++){
+// 			DeltaW3[j][i] = learningRate * H2[i] * nudgesHO[i] + learningRate * DeltaW3[i][j];
+// 			W3[j][i] += DeltaW3[j][i];
+// 		}
+// 	}
+// 	// W2
+// 	for(i = 0; i < HIDDEN; i++){
+// 		for(j = 0; j < HIDDEN; j++){
+// 			DeltaW2[j][i] = learningRate * H1[i] * nudgesHH[i] + learningRate * DeltaW2[i][j];
+// 			W2[j][i] += DeltaW2[j][i];
+// 		}
+// 	}
+
+// 	// W1
+// 	for(i = 0; i < INPUT; i++){
+// 		for(j = 0; j < HIDDEN; j++){
+// 			DeltaW1[j][i] = learningRate * input[i] * nudgesIH[i] + learningRate * DeltaW1[j][i];
+// 			W1[j][i] += DeltaW1[j][i];
+// 		}
+// 	}
+
+
+// 	// W3, BH2 is handled here.
+// 	// for(i = 0; i < OUTPUT; i++){
+// 	// 	learningValue 		 = sqrt(pow((nudgesHO[i]*learningRate), 2));	//Make a positive number.
+// 	// 	// learningValue 		 = 	 (nudges[i]*learningRate);
+// 	// 	// if(DEBUG) printf("\nNudge[%d] from output is: %f\n", i, nudges[i]);
+		
+// 	// 	// Start with W3, H2, BH2
+// 	// 	// The greater the influence of the node in H2, the greater the increase/decrease in value to maximize
+// 	// 	// the "bang for our bucks" on the weight/bias.
+// 	// 	for(j = 0; j < HIDDEN; j++){
+// 	// 		if(DEBUG) printf("Weight3 [%d][%d] was: %f\n", j, i, W3[j][i]);
+
+// 	// 		if(nudgesHO[i] < 0.0){							//If the nudge is negative, this is not the desired output. Decrease impact of edges.
+// 	// 			if(W3[j][i] < 0.0)
+// 	// 				W3[j][i] 		+= learningValue*Out[i];	//Negative becomes less negative.
+// 	// 			else 
+// 	// 				W3[j][i] 		-= learningValue*Out[i];	//Positive becomes less positive.
+
+// 	// 			// If H2[j] had a huge impact leading to a wrong answer (i.e. negative nudge), increase bias to make it less
+// 	// 			// likely to affect to a wrong answer again.
+// 	// 			BH2[j]				+= learningValue;
+// 	// 			// Remember desired changes
+// 	// 			layerNudges[j]		+= learningValue*W3[j][i];
+// 	// 		}else{											//If the nudge is positive, this is desired output. Increase positive impact of edges.
+// 	// 			W3[j][i] 			+= learningValue*Out[i];	//Positive becomes more positive.
+// 	// 			BH2[j]				-= learningValue;
+// 	// 			// Remember desired changes in proportion to the impact of a given weight.
+// 	// 			layerNudges[j]		+= learningValue*W3[j][i];
+// 	// 		}
+
+
+// 	// 		if(DEBUG) printf("Weight3 [%d][%d] is now: %f\n", j, i, W3[j][i]);
+			
+// 	// 	}
+
+// 	// 	if(DEBUG) printf("layerNudge[%d] is: %f\n", i, layerNudges[i]);
+// 	// }
+	
+// 	// if(DEBUG) printf("\n\n");
+
+// 	// // W2, H1, BH1 is handled here.
+// 	// for(i = 0; i < HIDDEN; i++){
+// 	// 	learningValue		 = sqrt(pow((layerNudges[i]*learningRate), 2));
+// 	// 	layerNudges[i]		 = 0.0;
+
+// 	// 	for(j = 0; j < HIDDEN; j++){
+// 	// 		if(DEBUG) printf("Weight2 [%d][%d] was: %f\n", j, i, W2[j][i]);
+
+// 	// 		if(layerNudges[i] < 0.0){							//If the nudge is negative, this is not the desired output. Decrease impact of edges.
+// 	// 			if(W2[j][i] < 0.0)
+// 	// 				W2[j][i] 		+= learningValue*H2[j];	//Negative becomes less negative.
+// 	// 			else 
+// 	// 				W2[j][i] 		-= learningValue*H2[j];	//Positive becomes less positive.
+
+// 	// 			// If H2[j] had a huge impact leading to a wrong answer (i.e. negative nudge), increase bias to make it less
+// 	// 			// likely to affect to a wrong answer again.
+// 	// 			BH1[j]				+= learningValue;
+// 	// 			// Remember desired changes
+// 	// 			layerNudges[j]		-= learningValue*W2[j][i];
+// 	// 		}else{											//If the nudge is positive, this is desired output. Increase impact of edges.
+// 	// 			W2[j][i] 			+= learningValue*H2[j];	//Positive becomes more positive.
+// 	// 			BH1[j]				-= learningValue;
+// 	// 			// Remember desired changes
+// 	// 			layerNudges[j]		+= learningValue*W2[j][i];
+// 	// 		}
+
+
+// 	// 		if(DEBUG) printf("Weight2 [%d][%d] is now: %f\n", j, i, W2[j][i]);
+// 	// 	}
+
+// 	// 	if(DEBUG) printf("layerNudge[%d] is: %f\n", i, layerNudges[i]);
+// 	// }
+
+// 	// if(DEBUG) printf("\n\n");
+
+// 	// // W1 is handeled here. Can only change W1 on this stage.
+// 	// for(i = 0; i < HIDDEN; i++){
+// 	// 	learningValue		 = sqrt(pow((layerNudges[i]*learningRate), 2));
+// 	// 	layerNudges[i]		 = 0.0;
+
+// 	// 	for(j = 0; j < HIDDEN; j++){
+// 	// 		if(DEBUG) printf("Weight1 [%d][%d] was: %f\n", j, i, W1[j][i]);
+
+// 	// 		if(layerNudges[i] < 0.0){
+// 	// 			if(W1[j][i] < 0.0)
+// 	// 				W1[j][i]		+= learningValue*H1[j];
+// 	// 			else
+// 	// 				W1[j][i] 		-= learningValue*H1[j];
+// 	// 		}else
+// 	// 			W1[j][i] 			+= learningValue*H1[j];
+			
+// 	// 		if(DEBUG) printf("Weight1 [%d][%d] is now: %f\n", j, i, W1[j][i]);
+// 	// 	}
+
+// 	// }
+// }
